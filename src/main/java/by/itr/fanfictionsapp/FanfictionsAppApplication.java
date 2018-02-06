bd5ac30e3b09febeaf4e3633ceb654094f478ca4
Alesk1 <zxcv9167@gmail.com>
2018-02-06 13:48:48 +0300

    Initial commit

---
+++ src/main/java/by/itr/fanfictionsapp/FanfictionsAppApplication.java
@@ -0,0 +1,12 @@
+package by.itr.fanfictionsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanfictionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanfictionsAppApplication.class, args);
+	}
+}
