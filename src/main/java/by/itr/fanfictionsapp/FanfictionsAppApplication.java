65a54aa6f5a6462db5f87fa628ab9627a3b3ad1d
Alesk1 <zxcv9167@gmail.com>
2018-02-11 19:48:23 +0300

    Changed war packaging to jar

--- src/main/java/by/itr/fanfictionsapp/FanfictionsAppApplication.java
+++ src/main/java/by/itr/fanfictionsapp/FanfictionsAppApplication.java
@@ -2,11 +2,14 @@ package by.itr.fanfictionsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class FanfictionsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FanfictionsAppApplication.class, args);
 	}
+
 }
